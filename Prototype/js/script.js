f1db27e32c7351e83dcf51dbda45b7fad754155d
aquilaneo <32867269+aquilaneo@users.noreply.github.com>
2018-11-21 19:33:33 +0900

    Three.jsテスト

---
+++ Prototype/js/script.js
@@ -0,0 +1,32 @@
+window.addEventListener ("load", init);
+
+function init () {
+	// canvasサイズ指定
+	const width = 960;
+	const height = 640;
+	
+	const renderer = new THREE.WebGLRenderer ({
+		canvas: document.querySelector ("#myCanvas")
+	});
+	renderer.setPixelRatio (window.devicePixelRatio);
+	renderer.setSize (width, height);
+	
+	const scene = new THREE.Scene ();
+	
+	const camera = new THREE.PerspectiveCamera (45, width / height);
+	camera.position.set (0, 0, 1000);
+	
+	const geometry = new THREE.BoxGeometry (400, 400, 400);
+	const material = new THREE.MeshNormalMaterial ();
+	const box = new THREE.Mesh (geometry, material);
+	scene.add (box);
+	
+	tick ();
+	
+	function tick () {
+		box.rotation.y += 0.01;
+		renderer.render (scene, camera);
+		
+		requestAnimationFrame (tick);
+	}
+}
